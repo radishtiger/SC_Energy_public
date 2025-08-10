f074136f68cb3e396c49b1411032c94090944c1b
radishtiger <69466028+radishtiger@users.noreply.github.com>
2025-08-10 13:30:00 +0900

    Clean comments and add setup docs

--- locate_and_edit/locate_modules/locate_by_subtraction.py
+++ locate_and_edit/locate_modules/locate_by_subtraction.py
@@ -2,23 +2,24 @@ import re
 import torch
 import torch.nn as nn
 
-class locate_by_subtraction():
+
+class locate_by_subtraction:
     def __init__(self, params, energynet):
         self.params = params
         self.energynet = energynet
-        self.softmax1 = nn.Softmax(dim = 1)
-        self.recursive_locate = True # determined by self.initialize()
+        self.softmax1 = nn.Softmax(dim=1)
+        self.recursive_locate = True  # determined by self.initialize()
         self.sep_token = "."
         self.cls_token = self.energynet.representation_model.tokenizer.cls_token
         # self.initialize()
 
     def initialize(self):
-        if 'recursive' in self.params['locate'].get('type', None).lower():
+        if "recursive" in self.params["locate"].get("type", None).lower():
             self.recursive_locate = True
         else:
             self.recursive_locate = False
 
-    def locate(self, pair, with_gold = True, threshold = None):
+    def locate(self, pair, with_gold=True, threshold=None):
         # inputs = [p[0] for p in pair]
         # errors = [p[1] for p in pair]
         # print("inputs:")
@@ -27,30 +28,32 @@ class locate_by_subtraction():
         # print(errors)
         # raise
         if self.recursive_locate == False:
-            locate_result = self.locate_only_once([p[0] for p in pair], with_gold, threshold)
+            locate_result = self.locate_only_once(
+                [p[0] for p in pair], with_gold, threshold
+            )
         else:
             locate_result = self.locate_without_gold([p[0] for p in pair], threshold)
 
         error_gold = [p[1] for p in pair]
-        
+
         pair_num = int(pair[0][0].count(self.sep_token))
-        locate_result['errors_gold'] = error_gold # batch 1짜리 취급
-        locate_result['pair_num'] = pair_num
+        locate_result["errors_gold"] = error_gold  # treat as a batch of size 1
+        locate_result["pair_num"] = pair_num
         return locate_result
 
-    def locate_only_once(self, pair, with_gold = True, threshold = None):
+    def locate_only_once(self, pair, with_gold=True, threshold=None):
         result = {}
         if with_gold:
             inputs = [p[0] for p in pair]
             errors = [p[1] for p in pair]
-            result['errors_gold'] = errors
+            result["errors_gold"] = errors
         else:
             inputs = pair
         prediction_list = self.locate_without_gold(inputs, threshold)
-        result['prediction_list'] = prediction_list
+        result["prediction_list"] = prediction_list
         return result
-        
-    def locate_without_gold(self, inputs, threshold = None):
+
+    def locate_without_gold(self, inputs, threshold=None):
         batch_size = len(inputs)
 
         prediction_list = []
@@ -60,32 +63,32 @@ class locate_by_subtraction():
             spans = self.detect_span(inputs[b])
             # print("[Area2]: detected spans")
             # for s in spans:
-                # print("\t",s)
-            pairs_dict = {s:i for i,s in enumerate(spans)}
+            # print("\t",s)
+            pairs_dict = {s: i for i, s in enumerate(spans)}
 
             inconsistent_pairs = self.locate_recursively(inputs[b], threshold, [])
             prediction_list.append([pairs_dict[p] for p in inconsistent_pairs])
 
-        return {'prediction_list': prediction_list}
-    
-    def locate_recursively(self, long_text, threshold = None, previous_returns = []):
+        return {"prediction_list": prediction_list}
+
+    def locate_recursively(self, long_text, threshold=None, previous_returns=[]):
         result = {}
-        
+
         # print("\n----")
         # print(f"stage {len(previous_returns)+1}: {long_text}")
         spans = self.detect_span(long_text)
         pair_num = len(spans)
 
-        e_val, _ = self.energynet.energy_model([long_text], pair_only = True)
+        e_val, _ = self.energynet.energy_model([long_text], pair_only=True)
         if threshold == None:
             threshold = self.energynet.threshold
-        
-        if self.energynet.output_form == 'real_num':
+
+        if self.energynet.output_form == "real_num":
             if float(e_val) < threshold or pair_num == 1:
                 # print("[Ended] This is consistent long_text.\n\n")
                 return previous_returns
-        if self.energynet.output_form == '2dim_vec':
-            if float(e_val[:,1]) < threshold or pair_num == 1:
+        if self.energynet.output_form == "2dim_vec":
+            if float(e_val[:, 1]) < threshold or pair_num == 1:
                 # print("[Ended] This is consistent long_text.\n\n")
                 return previous_returns
 
@@ -94,21 +97,23 @@ class locate_by_subtraction():
 
         # print("[Area3] subtracted inputs in recursive ft")
         # for s in subtracted_inputs:
-            # print("\t"*(len(previous_returns)+1), s)
-        
+        # print("\t"*(len(previous_returns)+1), s)
+
         for s_i in subtracted_inputs:
 
-            if self.energynet.output_form == 'real_num':
-                e_val, _ = self.energynet.energy_model([s_i], pair_only = True)
+            if self.energynet.output_form == "real_num":
+                e_val, _ = self.energynet.energy_model([s_i], pair_only=True)
                 values.append(float(e_val))
-            if self.energynet.output_form == '2dim_vec':
-                e_out, _ = self.energynet.energy_model([s_i], pair_only = True) # (len(set), 2)
-                probs_for_incon = e_out[:, 1] # shape = (len(set))
+            if self.energynet.output_form == "2dim_vec":
+                e_out, _ = self.energynet.energy_model(
+                    [s_i], pair_only=True
+                )  # (len(set), 2)
+                probs_for_incon = e_out[:, 1]  # shape = (len(set))
                 values.append(float(probs_for_incon))
 
         min_energy = min(values)
         prediction_int = list(values).index(min_energy)
-            
+
         # print(f"prediction_int(index, starts from 0): {prediction_int}")
         prediction_string = spans[prediction_int]
 
@@ -117,36 +122,36 @@ class locate_by_subtraction():
         # print("\t"*(len(previous_returns)+1), prediction_string)
         # print("\t"*(len(previous_returns)+1), subtracted_inputs[prediction_int])
 
-
         if threshold == None:
             return [prediction_string]
-        
+
         # elif min_energy < threshold or pair_num == 1:
         #     print(f"algorithm ended.")
         #     if min_energy < threshold:
         #         print("the remaining")
         #     return previous_returns + [prediction_string]
-        
+
         else:
             new_pair = subtracted_inputs[prediction_int]
-            return self.locate_recursively(new_pair, threshold, previous_returns + [prediction_string])
-        
+            return self.locate_recursively(
+                new_pair, threshold, previous_returns + [prediction_string]
+            )
 
-    def detect_span(self,set):
+    def detect_span(self, set):
 
         # set == text, e.g., '<s> qa pair 1 </s> qa pair 2 ... </s>
 
-        out = set[len(self.cls_token):].split(self.sep_token)[:-1]
-        
-        return [o.strip()+self.sep_token for o in out]
-    
+        out = set[len(self.cls_token) :].split(self.sep_token)[:-1]
+
+        return [o.strip() + self.sep_token for o in out]
+
     def subtract_input(self, spans):
 
         subtracted_list = []
         for i, s in enumerate(spans):
 
             sub_string = f"{self.cls_token} "
-            sub_string += ' '.join([spans[j] for j in range(len(spans)) if j !=i])
+            sub_string += " ".join([spans[j] for j in range(len(spans)) if j != i])
 
             subtracted_list.append(sub_string)
 
--- tasks/dataset_loader.py
+++ tasks/dataset_loader.py
@@ -7,64 +7,140 @@ from .vqa.lconvqa import l_convqa_dataset
 from .nli.set_nli import set_nli_dataset
 
 
-def dataset_loader(params, split = 'train', consistency = 'con', additional_info = {'pairwise': 'arbitrary_pairs', 'load_full': False}):
-    if params['task'] == 'vqa':
-        if params['dataset'] == 'lconvqa':
-            dataset = l_convqa_dataset(params, split, consistency, additional_info['pairwise'])
-        
-    elif params['task'] == 'multi-hop':
-        if params['dataset'] == 'musique':
+def dataset_loader(
+    params,
+    split="train",
+    consistency="con",
+    additional_info={"pairwise": "arbitrary_pairs", "load_full": False},
+):
+    if params["task"] == "vqa":
+        if params["dataset"] == "lconvqa":
+            dataset = l_convqa_dataset(
+                params, split, consistency, additional_info["pairwise"]
+            )
+
+    elif params["task"] == "multi-hop":
+        if params["dataset"] == "musique":
             dataset = musique_dataset(params, split)
-            
-    elif params['task'] == 'nli':
-        if params['dataset'] == 'set_nli':
-            dataset = set_nli_dataset(params, split, consistency, additional_info['pairwise'], additional_info['load_full'])
+
+    elif params["task"] == "nli":
+        if params["dataset"] == "set_nli":
+            dataset = set_nli_dataset(
+                params,
+                split,
+                consistency,
+                additional_info["pairwise"],
+                additional_info["load_full"],
+            )
 
     return dataset
 
-def dataset_loader_fine_grained(params, split = 'train', semantic = 'con', pairwise = "two_pairs", load_in_once_everything = False, output_form ='real_num', input_type = "set"):
-    if params['finetune_job_id'] == 0:
-        if params['task'] == "mixed":
+
+def dataset_loader_fine_grained(
+    params,
+    split="train",
+    semantic="con",
+    pairwise="two_pairs",
+    load_in_once_everything=False,
+    output_form="real_num",
+    input_type="set",
+):
+    if params["finetune_job_id"] == 0:
+        if params["task"] == "mixed":
             seq_of_dataset = []
             seq_of_data_name = []
-            if 'lconvqa' in params['dataset']:
-                seq_of_dataset.append(l_convqa_fine_grained_dataset(params, split, semantic, pairwise, load_in_once_everything, output_form, input_type))
-                seq_of_data_name.append('lconvqa')
-            if 'set_nli' in params['dataset']:
-                seq_of_dataset.append(set_nli_dataset(params, split, semantic, pairwise, load_in_once_everything, output_form, input_type))
-                seq_of_data_name.append('set_nli')
+            if "lconvqa" in params["dataset"]:
+                seq_of_dataset.append(
+                    l_convqa_fine_grained_dataset(
+                        params,
+                        split,
+                        semantic,
+                        pairwise,
+                        load_in_once_everything,
+                        output_form,
+                        input_type,
+                    )
+                )
+                seq_of_data_name.append("lconvqa")
+            if "set_nli" in params["dataset"]:
+                seq_of_dataset.append(
+                    set_nli_dataset(
+                        params,
+                        split,
+                        semantic,
+                        pairwise,
+                        load_in_once_everything,
+                        output_form,
+                        input_type,
+                    )
+                )
+                seq_of_data_name.append("set_nli")
 
             dataset = mixed_dataset(params, seq_of_dataset, seq_of_data_name)
 
-        elif params['task'] == 'vqa':
-            if params['dataset'] == 'lconvqa':
-                dataset = l_convqa_fine_grained_dataset(params, split, semantic, pairwise, load_in_once_everything, output_form, input_type)
+        elif params["task"] == "vqa":
+            if params["dataset"] == "lconvqa":
+                dataset = l_convqa_fine_grained_dataset(
+                    params,
+                    split,
+                    semantic,
+                    pairwise,
+                    load_in_once_everything,
+                    output_form,
+                    input_type,
+                )
 
-        elif params['task'] == 'multi-hop':
-            if params['dataset'] == 'musique':
+        elif params["task"] == "multi-hop":
+            if params["dataset"] == "musique":
                 dataset = musique_dataset(params, split)
 
-        elif params['task'] == 'nli':
-            if params['dataset'] == 'set_nli':
-                dataset = set_nli_dataset(params, split, semantic, pairwise, load_in_once_everything, output_form, input_type)
-    
-    
-    elif params['finetune_job_id'] != 0:
+        elif params["task"] == "nli":
+            if params["dataset"] == "set_nli":
+                dataset = set_nli_dataset(
+                    params,
+                    split,
+                    semantic,
+                    pairwise,
+                    load_in_once_everything,
+                    output_form,
+                    input_type,
+                )
+
+    elif params["finetune_job_id"] != 0:
         # We only consider two cases: from nli to lconvqa, from lconvqa to nli
         seq_of_dataset = []
         seq_of_data_name = []
-        seq_of_dataset.append(l_convqa_fine_grained_dataset(params, split, semantic, pairwise, load_in_once_everything, output_form, input_type))
-        seq_of_data_name.append('lconvqa')
-        seq_of_dataset.append(set_nli_dataset(params, split, semantic, pairwise, load_in_once_everything, output_form, input_type))
-        seq_of_data_name.append('set_nli')
+        seq_of_dataset.append(
+            l_convqa_fine_grained_dataset(
+                params,
+                split,
+                semantic,
+                pairwise,
+                load_in_once_everything,
+                output_form,
+                input_type,
+            )
+        )
+        seq_of_data_name.append("lconvqa")
+        seq_of_dataset.append(
+            set_nli_dataset(
+                params,
+                split,
+                semantic,
+                pairwise,
+                load_in_once_everything,
+                output_form,
+                input_type,
+            )
+        )
+        seq_of_data_name.append("set_nli")
 
         dataset = mixed_dataset(params, seq_of_dataset, seq_of_data_name)
-
 
     # dataset.dataset = [
     #     [
     #       (data_point1, consistency1), (data_point2, consistency2), ...
-    #     ], 
+    #     ],
     #     ....
     #   ]
     return dataset
@@ -73,14 +149,14 @@ def dataset_loader_fine_grained(params, split = 'train', semantic = 'con', pairw
 def transform_arbitrary_pairs_to_diverse_pairs(seq_of_dataset):
     """
 
-    seq_of_dataset = dataset들을 모아둔 것. set, tuple, list 모두 관계 없이 파이썬 상에서 iterable하기만 하면 됨.
-    이 함수에서는, arbitrary con_dataset과 arbitrary incon_dataset만 대입함.
-    이를 통해
-        
+    seq_of_dataset collects datasets; any iterable (set, tuple, list) works in Python.
+    This function uses only arbitrary con_dataset and arbitrary incon_dataset.
+    This produces datasets such as
+
         # size = 2
         two_pairs con
         two_pairs incon
-        
+
         # size = arbitrary
         con
         incon
@@ -95,19 +171,20 @@ def transform_arbitrary_pairs_to_diverse_pairs(seq_of_dataset):
         ...
         incon + incon + incon
 
-    이 종류들로 데이터셋들 만들음
+    Construct datasets of these types
 
 
     dataset: l_convqa_fine_grained_dataset.dataset
-        
+
     for d in dataset: # type(dataset) == list
         for data_point in d: # type(d) = list
             question, answer, consistency = data_point # type(data_point) = tuple
             # type(question): usually string
             # type(answer): usually string
             # type(consistency): bool. Consistency indicates whether this qa pair is consistent or inconsistent with respect to the rest.
-    
+
     """
+
 
 def transform_arbitrary_pairs_to_two_pairs(seq_of_dataset):
 
@@ -124,31 +201,43 @@ def transform_arbitrary_pairs_to_two_pairs(seq_of_dataset):
                     transformed_dataset_incon.append(data)
 
             else:
-                combinations = list(itertools.combinations(data, 2)) # "len(d)-choose-2"-number of combinations
+                combinations = list(
+                    itertools.combinations(data, 2)
+                )  # "len(d)-choose-2"-number of combinations
                 for two_pair in combinations:
                     if all([p[2] for p in two_pair]):
                         transformed_dataset_con.append(two_pair)
                     else:
                         transformed_dataset_incon.append(two_pair)
-            
+
             if len(data) not in set_sizes:
                 set_sizes[len(data)] = 0
-            set_sizes[len(data)] +=1
-
+            set_sizes[len(data)] += 1
 
     random.Random(1014).shuffle(transformed_dataset_con)
     random.Random(1014).shuffle(transformed_dataset_incon)
 
-    transformed_dataset_con = transformed_dataset_con[:min([len(s) for s in seq_of_dataset])]
-    transformed_dataset_incon = transformed_dataset_incon[:min([len(s) for s in seq_of_dataset])]
+    transformed_dataset_con = transformed_dataset_con[
+        : min([len(s) for s in seq_of_dataset])
+    ]
+    transformed_dataset_incon = transformed_dataset_incon[
+        : min([len(s) for s in seq_of_dataset])
+    ]
 
-    return [general_dataset(transformed_dataset_con), general_dataset(transformed_dataset_incon)], ['con_two_pairs', 'incon_two_pairs'], set_sizes
+    return (
+        [
+            general_dataset(transformed_dataset_con),
+            general_dataset(transformed_dataset_incon),
+        ],
+        ["con_two_pairs", "incon_two_pairs"],
+        set_sizes,
+    )
 
-def concat_arbitrary_pairs(seq_of_dataset, concat_num = 2):
+
+def concat_arbitrary_pairs(seq_of_dataset, concat_num=2):
     """
-    seq_of_dataset = [con_dataset, incon_dataset] 딱 두 개
+    seq_of_dataset = [con_dataset, incon_dataset] exactly two
     """
-
 
     set_sizes = {}
     data_num = min([len(s) for s in seq_of_dataset])
@@ -158,20 +247,24 @@ def concat_arbitrary_pairs(seq_of_dataset, concat_num = 2):
     total_dataset = []
     dataset_names = []
 
-    semantic_combinations = [(concat_num-i, i) for i in range(concat_num+1)] # con과 incon을 몇대몇 비율로 섞을 것인지에 대한 모든 경우의 수 나열
+    semantic_combinations = [
+        (concat_num - i, i) for i in range(concat_num + 1)
+    ]  # enumerate all con/incon mixing ratios
     for s_comb in semantic_combinations:
         one_dataset = []
         random.Random(1014).shuffle(index_combinations)
-        idx_combs = index_combinations[:data_num] # e.g., concat_num=3일 때, idx_combs = [(1,2,3), (3,6,2), ...]
+        idx_combs = index_combinations[
+            :data_num
+        ]  # e.g., when concat_num=3, idx_combs = [(1,2,3), (3,6,2), ...]
 
-        con_num, incon_num = s_comb # e.g., con_num, incon_num = (2, 1)
-        dataset_names.append('_'.join(['con'] * con_num + ['incon'] * incon_num))
+        con_num, incon_num = s_comb  # e.g., con_num, incon_num = (2, 1)
+        dataset_names.append("_".join(["con"] * con_num + ["incon"] * incon_num))
 
         data_name = []
         for indices in idx_combs:
-            tmp = [] # one data. 이게 데이터포인트 하나로 간주될 것임
+            tmp = []  # one data. Treated as a single data point
 
-            for semantic_idx, data_idx in enumerate(indices): # e.g., indices = (2,7,6)
+            for semantic_idx, data_idx in enumerate(indices):  # e.g., indices = (2,7,6)
                 if semantic_idx + 1 <= con_num:
                     tmp += seq_of_dataset[0][data_idx]
                 else:
@@ -180,13 +273,13 @@ def concat_arbitrary_pairs(seq_of_dataset, concat_num = 2):
 
             if len(tmp) not in set_sizes:
                 set_sizes[len(tmp)] = 0
-            set_sizes[len(tmp)] +=1
+            set_sizes[len(tmp)] += 1
             one_dataset.append(tmp)
-            if hasattr(seq_of_dataset[0], 'data_name'):
+            if hasattr(seq_of_dataset[0], "data_name"):
                 data_name.append(seq_of_dataset[0].data_name[data_idx])
-        
+
         gen_dataset = general_dataset(one_dataset)
-        if hasattr(seq_of_dataset[0], 'data_name'):
+        if hasattr(seq_of_dataset[0], "data_name"):
             gen_dataset.data_name = data_name
         else:
             gen_dataset.data_name = None
@@ -195,7 +288,7 @@ def concat_arbitrary_pairs(seq_of_dataset, concat_num = 2):
 
     return total_dataset, dataset_names, set_sizes
 
-    
+
 class general_dataset(Dataset):
     def __init__(self, dataset):
         self.dataset = dataset
@@ -203,10 +296,11 @@ class general_dataset(Dataset):
 
     def __getitem__(self, index):
         return self.dataset[index]
-    
+
     def __len__(self):
         return len(self.dataset)
-    
+
+
 class mixed_dataset(Dataset):
     def __init__(self, params, seq_of_dataset, seq_of_data_name):
         self.params = params
@@ -216,21 +310,18 @@ class mixed_dataset(Dataset):
         self.dataset = []
         self.data_name = []
         self.initialize()
-        
+
     def initialize(self):
         data = []
         for data_name, dataset in zip(self.seq_of_data_name, self.seq_of_dataset):
-           data += [
-                (d, data_name) for d in dataset.dataset
-            ]
+            data += [(d, data_name) for d in dataset.dataset]
 
         random.Random(self.seed).shuffle(self.dataset)
         self.data_name = [d[1] for d in data]
         self.dataset = [d[0] for d in data]
 
-
     def __getitem__(self, index):
         return self.dataset[index]
-    
+
     def __len__(self):
         return len(self.dataset)
